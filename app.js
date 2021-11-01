af2700524fb93a5ba495df21104a4bd84f5a5d10
Ayowalexy <seinde4@gmail.com>
2021-11-01 23:54:32 +0100

    Uploaded ETB backend code

---
+++ app.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const express = require('express')
+const app = express()
+const Stripe = require('stripe')
+
+
+const stripePublishableKey = process.env.STRIPE_PUBLISHABLE_KEY;
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+
+const stripe = Stripe(stripeSecretKey, {apiVersion: "2020-08-27"})
+
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+app.post('/creat-payment-intent/:amount', async (req, res) => {
+    const amount = req.params.amount * 100
+    try {
+
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: 'usd',
+            payment_method_types: ['card']
+        })
+
+        const clientSecret = paymentIntent.client_secret
+
+        res.json({
+            clientSecret: clientSecret
+        })
+    } catch (e){
+        console.log(e.message)
+        res.json({error: e.message})
+
+    }
+})
+
+app.listen(8080, () => console.log('Listening on Port 8080'))
